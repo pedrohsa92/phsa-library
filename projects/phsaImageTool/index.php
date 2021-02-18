d5565aa3d481d47d50a78f36c750faaec3f93138
Pedro HSA <pedro.hsa92@gmail.com>
2021-02-18 15:09:17 -0300

    mostrando o alinhamento de imagens

--- projects/phsaImageTool/index.php
+++ projects/phsaImageTool/index.php
@@ -12,13 +12,110 @@
     <link rel="stylesheet" type="text/css" href="css/phsaImageTool.css">
 </head>
 <body>
-	<div class="container pt-5 mt-5">
+	<div class="container pt-5 mt-5 mb-5 pb-5">
 		<h1 class="text-center">PHSA IMAGE <i>TOOL</i></h1>
 
 		<p class="text-primary mb-5 h5 text-center">
 			Uma biblioteca para alinhamento de imagem!
 		</p>
+
+		<div class="row mb-3">
+			<div class="col-md-4 bg-hover-secondary" align="center">
+				<h6 class="text-secondary">col-md-4 imgtool-200</h6>
+				<div class="imgtool-200 border border-info">
+					<div class="imgtool_mold"><img src="media/beatles-1024x673.jpg"></div>
+				</div>
+			</div>
+			<div class="col-md-4 bg-hover-secondary" align="center">
+				<h6 class="text-secondary">col-md-4 imgtool-200</h6>
+				<div class="imgtool-200 border border-info">
+					<div class="imgtool_mold"><img src="media/cristo-redentor-400x600.jpg"></div>
+				</div>
+			</div>
+			<div class="col-md-4 bg-hover-secondary" align="center">
+				<h6 class="text-secondary">col-md-4 imgtool-200</h6>
+				<div class="imgtool-200 border border-info">
+					<div class="imgtool_mold"><img src="media/einstein-610x486.jpg"></div>
+				</div>
+			</div>
+		</div>
+
+		<hr>
+
+		<div class="row mb-3">
+			<div class="col-md-4 bg-hover-secondary" align="center">
+				<h6 class="text-secondary">col-md-4 imgtool-150</h6>
+				<div class="imgtool-150 border border-info">
+					<div class="imgtool_mold"><img src="media/miles-morales-spider-man-728x1300.jpg"></div>
+				</div>
+			</div>
+			<div class="col-md-4 bg-hover-secondary" align="center">
+				<h6 class="text-secondary">col-md-4 imgtool-150</h6>
+				<div class="imgtool-150 border border-info">
+					<div class="imgtool_mold"><img src="media/tesla-1000x1000.jpg"></div>
+				</div>
+			</div>
+			<div class="col-md-4 bg-hover-secondary" align="center">
+				<h6 class="text-secondary">col-md-4 imgtool-150</h6>
+				<div class="imgtool-150 border border-info">
+					<div class="imgtool_mold"><img src="media/torre-eiffel-1200x1800.jpg"></div>
+				</div>
+			</div>
+		</div>
+
+		<hr>
+
+		<div class="row mb-3">
+			<div class="col-md-4 bg-hover-secondary" align="center">
+				<h6 class="text-secondary">col-md-4 imgtool-100</h6>
+				<div class="imgtool-100 border border-info">
+					<div class="imgtool_mold"><img src="media/beatles-1024x673.jpg"></div>
+				</div>
+			</div>
+			<div class="col-md-4 bg-hover-secondary" align="center">
+				<h6 class="text-secondary">col-md-4 imgtool-100</h6>
+				<div class="imgtool-100 border border-info">
+					<div class="imgtool_mold"><img src="media/cristo-redentor-400x600.jpg"></div>
+				</div>
+			</div>
+			<div class="col-md-4 bg-hover-secondary" align="center">
+				<h6 class="text-secondary">col-md-4 imgtool-100</h6>
+				<div class="imgtool-100 border border-info">
+					<div class="imgtool_mold"><img src="media/einstein-610x486.jpg"></div>
+				</div>
+			</div>
+		</div>
+
+		<hr>
+
+		<div class="row mb-3">
+			<div class="col-md-4 bg-hover-secondary" align="center">
+				<h6 class="text-secondary">col-md-4 imgtool-50</h6>
+				<div class="imgtool-50 border border-info">
+					<div class="imgtool_mold"><img src="media/miles-morales-spider-man-728x1300.jpg"></div>
+				</div>
+			</div>
+			<div class="col-md-4 bg-hover-secondary" align="center">
+				<h6 class="text-secondary">col-md-4 imgtool-50</h6>
+				<div class="imgtool-50 border border-info">
+					<div class="imgtool_mold"><img src="media/tesla-1000x1000.jpg"></div>
+				</div>
+			</div>
+			<div class="col-md-4 bg-hover-secondary" align="center">
+				<h6 class="text-secondary">col-md-4 imgtool-50</h6>
+				<div class="imgtool-50 border border-info">
+					<div class="imgtool_mold"><img src="media/torre-eiffel-1200x1800.jpg"></div>
+				</div>
+			</div>
+		</div>
+
 	</div>
+
+	<style type="text/css">
+		.bg-hover-secondary:hover{
+			background: rgba(0, 0, 0, 0.1);
+		}
+	</style>
 
 	<!-- Jquery and Bootstrap -->
     <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
